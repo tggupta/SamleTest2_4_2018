5c98b55e7ec7c6f24b1ca72239557ca67607ee83
Tarun <Tarun@192.168.0.5>
2018-02-04 17:59:00 +0530

    1st commit
    Signed-off-by: Tarun <Tarun@192.168.0.5>

---
+++ src/browser_Test/browserTest.java
@@ -0,0 +1,20 @@
+package browser_Test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class browserTest {
+
+	WebDriver driver;
+
+	@Test
+	public void gettest()
+	{
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Tarun\\workspace\\SamleTest2_4_2018\\lib\\chromedriver1.exe");
+		driver=new ChromeDriver();
+		
+		driver.get("http://www.flipkart.com");
+	}
+	
+}
